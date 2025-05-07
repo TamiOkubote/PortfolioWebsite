74d393e3a1d8dbf6449379b48f818d7f062b0f74
TamiOkubote <tspeedstar@icloud.com>
2025-05-07 19:50:25 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const footer = document.querySelector("footer");
+    footer.style.position = "relative";
+    footer.style.bottom = "0";
+});
